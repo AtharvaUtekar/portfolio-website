70458da41b5d0e360e1850d085cd0018a78f10c7
AtharvaUtekar <atharva.v.utekar@gmail.com>
2023-04-08 13:49:18 +0530

    Updated desktop loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+  <div className="justify-center items-center content-center pt-[450px] md:pt-[600px] lg:pt-[32%] flex">
+  <span className="justify-center items-center content-center flex blur-sm animate-ping shadow-xl absolute h-16 w-16 rounded-full bg-white opacity-65"></span>
+
+  </div>
   )
 }
 
