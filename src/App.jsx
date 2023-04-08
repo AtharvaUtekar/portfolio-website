2ed04e2fd181e94b6ec3e745406e71522b6b1649
AtharvaUtekar <atharva.v.utekar@gmail.com>
2023-04-08 13:48:02 +0530

    Updated styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter } from 'react-router-dom'
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works} from './components'
 import Stars from './components/canvas/Stars'
 
+// 57:40 -- For 3D models skecthfab- > downloadable the deslktop is 3D model
+// 
 const App = () => {
   return (
     <BrowserRouter>
     <div className="relative z-0 bg-primary">
-      <div className='bg-hero-pattern bg-cover bg-no-repeat'>
-        <Navbar/>
-        <Hero/>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center mb-12'>
+        <Navbar />
+        <Hero />
       </div>
       <About/>
       <Experience/>
